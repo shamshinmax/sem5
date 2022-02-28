5769477125898d91aa3d0fd690577fe513bae673
shamshinmax <mksmsmsn@gmail.com>
2022-02-28 21:31:37 +0300

    last

--- view.py
+++ view.py
@@ -1,6 +1,6 @@
 def viewer(result):
     print(f"result = {result}")
 def get_int():
-    return int(input("value = "))
+    return float(input("value = "))
 def get_complex():
     return complex(int(input("value =")), int(input("value =")))
